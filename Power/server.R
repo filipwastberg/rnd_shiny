090e8f835ac93ce192215f6c5fa285f5ebcd667c
filipwastberg <filip.wastberg@ferrologic.se>
2018-05-29 15:03:26 +0200

    lite korr

--- Power/server.R
+++ Power/server.R
@@ -9,19 +9,19 @@
 library(shiny)
 # Define server logic required to draw a histogram
 function(input, output){
- textinput <- reactive({
-   res1 <-  (-qnorm(1-input$sig1/100)+qnorm(input$Power))^2
-   res2 <-  input$hitrate1*(1-input$hitrate1)+input$hiteratec1*(1-input$hiteratec1)
-   res3 <- (input$hitrate1-input$hiteratec1)^2
-   res4 <- round(res1*res2/res3,0)
-   res5 <- input$pros1 - res4
-   alpha <- (1-input$sig1/100)
-  kontrollgrupp <-as.integer(ifelse(res5 < res4, input$pros1/2, res4))
-   malgrupp <- as.integer(input$pros1-kontrollgrupp)
+ textinput < -  reactive({
+   res1 < -   ( - qnorm(1 - input$sig1/100) + qnorm(input$Power))^2
+   res2 < -   input$hitrate1*(1 - input$hitrate1) + input$hiteratec1*(1 - input$hiteratec1)
+   res3 < -  (input$hitrate1 - input$hiteratec1)^2
+   res4 < -  round(res1*res2/res3,0)
+   res5 < -  input$pros1  -  res4
+   alpha < -  (1 - input$sig1/100)
+  kontrollgrupp < -  as.integer(ifelse(res5 < res4, input$pros1/2, res4))
+   malgrupp < -  as.integer(input$pros1 - kontrollgrupp)
    
-   tab <- as.data.frame(cbind(malgrupp, kontrollgrupp))
-   names(tab) <- c("Målgrupp 1", "Kontrollgrupp 1")
+   tab < -  as.data.frame(cbind(malgrupp, kontrollgrupp))
+   names(tab) < -  c("Målgrupp 1", "Kontrollgrupp 1")
    return(tab)
  })
-  output$trams <- renderTable({textinput()})
+  output$trams < -  renderTable({textinput()})
 }
