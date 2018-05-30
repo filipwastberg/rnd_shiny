bdecd970c4768ae279dbed554fa25782968452dd
Carl Hogfeldt <carl.hogfeldt@ferrologic.se>
2018-05-30 10:30:43 +0200

    stuff

--- Power/ui.R
+++ Power/ui.R
@@ -1,11 +1,3 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
 
 library(shiny)
 library(shinydashboard)
@@ -13,31 +5,25 @@ library(shinydashboard)
 dashboardPage(
   dashboardHeader(title = "Beräkning av kontrollgruppsstorlek för att bevisa en skillnad mellan grupperna"),
   dashboardSidebar("Teststyrka",
-               numericInput("test","Antal test", value = 1),
-               sliderInput("sig1", "Signifikansnivå",
-                           min = 1, max = 99, value = 95),
-               sliderInput("Power", "Power", min=0.01, max=0.99, value=0.8),
-               "Mål och kontrollgrupp 1",
-               numericInput("pros1","Totalt antal prospekts",value=10000),
-               numericInput("hitrate1", "Hit rate målgrupp", value=0.01),
-               numericInput("hiteratec1", "Hit rate kontrollgruppen", value=0.005),
-               "Mål och kontrollgrupp 2",
-               numericInput("pros2","Totalt antal prospekts",value=NA),
-               numericInput("hitrate2", "Hit rate målgrupp", value=NA, min=0, max=1),
-               numericInput("hiteratec2", "Hit rate kontrollgruppen", value=NA, min=0, max=1)
-
+                   numericInput("test","Antal test", value = 1),
+                   sliderInput("sig1", "Signifikansnivå",
+                               min = 1, max = 99, value = 95),
+                   sliderInput("Power", "Power", min=0.01, max=0.99, value=0.8),
+                   "Mål och kontrollgrupp 1",
+                   numericInput("pros1","Totalt antal prospekts",value=10000),
+                   numericInput("hitrate1", "Hit rate målgrupp", value=0.01),
+                   numericInput("hiteratec1", "Hit rate kontrollgruppen", value=0.005),
+                   "Mål och kontrollgrupp 2",
+                   numericInput("pros2","Totalt antal prospekts",value=NA),
+                   numericInput("hitrate2", "Hit rate målgrupp", value=NA, min=0, max=1),
+                   numericInput("hiteratec2", "Hit rate kontrollgruppen", value=NA, min=0, max=1),             
+                   img(src='logo.jpg', align = "right")
+                   
   )
   ,
   dashboardBody(
     tableOutput("trams" ),
     tableOutput("trams1" ),
-    tableOutput("trams2" ),
-    
-    HTML('<header>
- <img src="logo.jpg",height="120", width="120"</img>
-     
-     </header>')
+    tableOutput("trams4" )
     
   ))
-
-
